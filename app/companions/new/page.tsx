2bd3771254011a2d937c060fcecac9d0fcfaea8c
theenesh <theenesh7073@gmail.com>
2025-06-16 23:47:29 +0530

    implement supabase

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,13 +1,20 @@
 import CompanionForm from "@/components/CompanionForm"
-const NewCompanion = () => {
-  return (
-    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-     <article className="w-full gap-4 flex flex-col">
-      <h1>Companion Builder</h1>
-      <CompanionForm />
-      </article>
-    </main>
-  )
+import {auth} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
+import Link from "next/link";
+const NewCompanion = async () => {
+  const { userId } = await auth();
+    if(!userId) redirect('/sign-in');
+
+    const canCreateCompanion = await auth()
+    return (
+        <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+            <article className="w-full gap-4 flex flex-col">
+                <h1>Companion Builder</h1>
+                <CompanionForm />
+            </article>
+        </main>
+    )
 }
 
 export default NewCompanion
--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -23,7 +23,7 @@ import {
 } from "@/components/ui/select"
 import {subjects} from "@/constants";
 import {Textarea} from "@/components/ui/textarea";
-//import {createCompanion} from "@/lib/actions/companion.actions";
+import {createCompanion} from "@/lib/actions/companion.actions";
 import {redirect} from "next/navigation";
 
 const formSchema = z.object({
@@ -49,7 +49,14 @@ const CompanionForm = () => {
     })
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+ const companion = await createCompanion(values);
+
+        if(companion) {
+            redirect(`/companions/${companion.id}`);
+        } else {
+            console.log('Failed to create a companion');
+            redirect('/');
+        }
     }
 
     return (

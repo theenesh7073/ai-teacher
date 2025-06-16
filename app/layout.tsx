2542207635a2d59463bea7a01a1d8126cc03f59b
theenesh <theenesh7073@gmail.com>
2025-06-16 16:25:41 +0530

    implemnt login auth using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Variable } from "lucide-react";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -21,9 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
+        <ClerkProvider appearance={{variables: { colorPrimary: "#fec32a"}} }>
+                <Navbar />
         {children}
-        </body>
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -8,7 +8,6 @@ const navItems = [
     { label:'Home', href: '/' },
     { label: 'Companions', href: '/companions' },
     { label: 'My Journey', href: 'my-journey' },
-    { label: 'Sign in', href: 'sign-in' },
 ]
 
 const NavItems = () => {
